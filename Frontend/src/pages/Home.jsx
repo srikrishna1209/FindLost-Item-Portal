e27a63bc3233a493f01fd92b1705e32c9b3be9d6
Srikrishna Jogi <srikrishna.jogi09@gmail.com>
2026-08-09 11:25:45 +0530

    Set up frontend routing and pages

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Find Lost Item Portal</h1>
+      <p>Find what you lost. Return what you found.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Registration page</p>
+    </div>
+  );
+}
+
+export default Register;
